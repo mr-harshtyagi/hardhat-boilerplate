fe0214c4f9af4232778f500d231df2e66b22ae8b
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-08-01 12:11:59 +0530

    add testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,25 @@
-const { task } = require("hardhat/config");
-
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+const { task } = require("hardhat/config")
+require("@nomicfoundation/hardhat-toolbox")
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
-require("./tasks/faucet");
+require("./tasks/faucet")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 task("accounts", "Prints the list of accounts", async function (taskArgs, hre) {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const GOERLI_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
   solidity: "0.8.9",
@@ -23,5 +27,15 @@ module.exports = {
     hardhat: {
       chainId: 1337, // We set 1337 to make interacting with MetaMask simpler
     },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
   },
-};
+}
